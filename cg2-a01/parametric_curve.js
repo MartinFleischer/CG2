66dfed999e71e844533cfe9b89ab5e671dd38801
norman-codekitchen <norman.reszka@gmail.com>
2012-10-16 10:40:15 +0200

    added parametric_curve

---
+++ cg2-a01/parametric_curve.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+/* requireJS module definition */
+define(["util","vec2","scene","point_dragger"],
+		(function(Util, vec2, Scene, PointDragger){
+	
+	"use strict";
+	
+	var ParametricCurve = function(point1, point2, lineStyle){
+		
+	};
+}));
